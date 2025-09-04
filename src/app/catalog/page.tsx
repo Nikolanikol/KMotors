47e46e75e096abd3ab74a100589de771df547e8d
Nikolai <krezi3927@gmail.com>
2025-09-04 13:15:58 +0900

    iframe added

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const pages = () => {
-  return <div>catalog</div>;
+  return (
+    <div className="min-h-screen border-2 border-black">
+      {" "}
+      <iframe
+        src="https://carnex.vercel.app/"
+        className="w-full min-h-screen border-black border-2"
+        loading="lazy"
+        title="Каталог"
+      />{" "}
+    </div>
+  );
 };
 
 export default pages;
