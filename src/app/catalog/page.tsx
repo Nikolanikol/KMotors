91a7a7e2aa90d2a2319c31be82d5db0640624eab
Николай Борис <krezi3927@gmail.com>
2025-09-14 13:50:42 +0900

    'brand'

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React from "react";
-
+import { useSearchParams } from "next/navigation";
 const pages = () => {
   const mode = process.env.NEXT_PUBLIC_DEPLOY_MODE;
+
+  const searchParams = useSearchParams();
+  const manufacture = searchParams.get("manufacture");
+  // console.log(manufacture); //здесь приняли параметр  теперь надо его прокинуть в iframe
   return (
     <div className="min-h-screen ">
       {" "}
@@ -10,7 +14,7 @@ const pages = () => {
         src={
           mode === "production"
             ? "https://carnex.vercel.app/"
-            : "http://localhost:5173/"
+            : `http://localhost:5173?manufacture=${manufacture}`
         }
         className="w-full min-h-screen "
         loading="lazy"
--- src/components/Home/Brands.tsx
+++ src/components/Home/Brands.tsx
@@ -17,18 +17,57 @@ import porsche from "./logo/porsche.webp";
 
 import ssangyong from "./logo/ssangyong.webp";
 import renaultSamsung from "./logo/renault-samsung.webp";
+import Link from "next/link";
 
 const Brands = () => {
-  const logoArray: { logo: StaticImageData; name: string }[] = [
-    { logo: kia, name: "Kia" },
-    { logo: audi, name: "Audi" },
-    { logo: bmw, name: "BMW" },
-    { logo: chevrolet, name: "Chevrolet" },
-    { logo: hyundai, name: "Hyundai" },
-    { logo: mercedesBenz, name: "Mercedes-Benz" },
-    { logo: porsche, name: "Porsche" },
-    { logo: ssangyong, name: "SsangYong" },
-    { logo: renaultSamsung, name: "Renault Samsung" },
+  const logoArray: { logo: StaticImageData; name: string; action: string }[] = [
+    {
+      logo: kia,
+      name: "Kia",
+      action: "(And.Hidden.N._.(C.CarType.Y._.Manufacturer.기아.))",
+    },
+    {
+      logo: audi,
+      name: "Audi",
+      action: "(And.Hidden.N._.(C.CarType.N._.Manufacturer.아우디.))",
+    },
+    {
+      logo: bmw,
+      name: "BMW",
+      action: "(And.Hidden.N._.(C.CarType.N._.Manufacturer.BMW.))",
+    },
+    {
+      logo: chevrolet,
+      name: "Chevrolet",
+      action: "(And.Hidden.N._.(C.CarType.Y._.Manufacturer.쉐보레(GM대우_).))",
+    },
+    {
+      logo: hyundai,
+      name: "Hyundai",
+      action: "(And.Hidden.N._.(C.CarType.Y._.Manufacturer.현대.))",
+    },
+    {
+      logo: mercedesBenz,
+      name: "Mercedes-Benz",
+      action: "(And.Hidden.N._.(C.CarType.N._.Manufacturer.벤츠.))",
+    },
+    {
+      logo: porsche,
+      name: "Porsche",
+      action: "(And.Hidden.N._.(C.CarType.N._.Manufacturer.포르쉐.)) ",
+    },
+    {
+      logo: ssangyong,
+      name: "SsangYong",
+      action:
+        "(And.Hidden.N._.(C.CarType.Y._.Manufacturer.KG모빌리티(쌍용_).))",
+    },
+    {
+      logo: renaultSamsung,
+      name: "Renault Samsung",
+      action:
+        "(And.Hidden.N._.(C.CarType.Y._.Manufacturer.르노코리아(삼성_).))",
+    },
   ];
 
   kia;
@@ -37,10 +76,20 @@ const Brands = () => {
       <div className="flex flex-col justify-center gap-30">
         <h2 className="heading-2 text-center">Brands</h2>
         <div className="carouselwrapper  min-h-24 ">
-          <Carousel>
-            <CarouselContent className="flex gap-x-3 py-4">
-              {logoArray.map(({ logo, name }, index) => (
-                <BrandCard key={index} logo={logo} name={name} />
+          <Carousel orientation="horizontal" className="px-5">
+            <CarouselContent className="flex gap-x-3 py-4 px-3">
+              {logoArray.map(({ logo, name, action }, index) => (
+                <Link
+                  className="basis-1/4 shrink-0  rounded-2xl shadow-2xl flex items-center justify-center py-2 px-4 bg-white hover:scale-105 transition-all duration-300"
+                  key={index}
+                  href={{
+                    pathname: "/catalog",
+                    query: { manufacture: action },
+                  }}
+                >
+                  {" "}
+                  <BrandCard logo={logo} name={name} />
+                </Link>
               ))}
             </CarouselContent>
             <div>
@@ -62,7 +111,7 @@ interface BrandCardProps {
 }
 const BrandCard = ({ logo, name }: BrandCardProps) => {
   return (
-    <CarouselItem className="basis-1/4  rounded-2xl shadow-2xl flex items-center justify-center py-2 px-4 bg-white ">
+    <CarouselItem className="flex justify-center pl-0 text-center">
       <div className="flex flex-col gap-2 items-center justify-between  ">
         <Image height={150} src={logo} alt="logo" />
         <h4 className="font-bold text-xl">{name}</h4>
