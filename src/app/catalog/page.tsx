5c4788dcb8f5216c8ffb83311eb7b3e5dbcad354
Nikolai <krezi3927@gmail.com>
2025-10-04 16:50:04 +0900

    deploi main

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -11,55 +11,11 @@ import { CarSearchParams } from "@/components/Catalog/Row/utils/Types";
 // carString
 // https://api.encar.com/search/car/list/general?count=true&q=${query}&inav=%7CMetadata%7CSort
 
-const fetchCatalog = async (query: string = "(And.Hidden.N._.CarType.Y.)") => {
-  try {
-    const res = await fetch(
-      `https://api.encar.com/search/car/list/general?count=true&q=${query}&inav=%7CMetadata%7CSort`,
-      {
-        headers: {
-          "user-agent":
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-        },
-      }
-    )
-      .then((data) => data.json())
-      .then((res) => {
-        const data = res.iNav.Nodes.find(
-          (i: any) => i.DisplayName === "국산여부"
-        ).Facets.find((i: any) => i.IsSelected === true).Refinements.Nodes[0]
-          .Facets;
-
-        return data;
-      });
-
-    return res;
-  } catch (error) {
-    const res =
-      fetch(`https://encar-proxy-main.onrender.com/api/nav?count=true&q=${"(And.Hidden.N._.CarType.N.)"}&inav=%7CMetadata%7CSort
-`)
-        .then((data) => data.json())
-
-        .then((res) => {
-          const data = res.iNav.Nodes.find(
-            (i: any) => i.DisplayName === "국산여부"
-          ).Facets.find((i: any) => i.IsSelected === true).Refinements.Nodes[0]
-            .Facets;
-          //   console.log(data);
-          return data;
-        });
-    return res;
-  }
-
-  // .catch(()=>{
-};
 export default async function ({
   searchParams,
 }: {
   searchParams: CarSearchParams;
 }) {
-  //   const filterData = await fetchCatalog(searchParams);
-
-  //   console.log(filterData); // filter data
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 p-2 h-full m-0 mx-auto">
       {/* каталога */}
