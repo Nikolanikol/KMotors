3bb293627572b8fb60e5047a9c8f310c2e641d69
Nikolai <krezi3927@gmail.com>
2025-10-08 14:23:52 +0900

    seo next

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["ci.encar.com"],
   },
-//     eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//    typescript: {
-//     ignoreBuildErrors: true,
-//   },
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
+   typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
